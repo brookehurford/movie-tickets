e3dba78f6c36449c7d9916fa9f580c91122e0686
brooke hurford and jessica fix <jcfix.pdx@gmail.com>
2016-01-26 10:45:04 -0800

    Add initial files and first passing spec

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Business Logic //
+
+function ticketPrice(times, age) {
+  this.times = [];
+  this.age = ("adult");
+
+}
+
+// Contact.prototype.fullName = function() {
+//   return this.firstName + " " + this.lastName;
+// }
---
+++ spec/specs.js
@@ -0,0 +1,23 @@
+describe('ticketPrice', function() {
+  it('will create a new price with the following properties', function(){
+    var testPrice = new ticketPrice("time", "age");
+    expect(testPrice.times).to.eql([]);
+    expect(testPrice.age).to.equal("adult");
+  });
+});
+
+
+
+
+
+
+
+// Movie Tickets //
+//var ticketPrice =
+//timeOfDay
+//discountCitizen
+//firstRunMovies
+
+
+
+//it will show a ticket price with the following properties:
