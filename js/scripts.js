ff6b7fc55b69b09545b8a0c2f350f33ea491c4e7
brooke hurford and jessica fix <brookebhurford@gmail.com>
2016-01-26 16:26:18 -0800

    Make the form work to spit out the correct price

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,49 @@
 // Business Logic //
-
-function Ticket(movieTitle, times, age) {
+function Ticket(movieTitle, times, age, ticketPrice) {
   this.movieTitle = movieTitle;
   this.times = times;
   this.age = age;
+  this.ticketPrice = ticketPrice;
 }
 
 Ticket.prototype.price = function() {
-  debugger;
-  if ((this.age === "adult") && (this.times === "1:00pm")) {
-    return 8;
+
+  if ((this.age === "adult") && (this.times === "matinee")) {
+    return this.ticketPrice = 8;
   }
-  else if ((this.age === "honoredCitizen") && (this.times === "5:00pm")) {
-    return 5;
+  else if ((this.age === "honoredCitizen") && (this.times === "evening")) {
+    return this.ticketPrice = 5;
   }
-  else if ((this.age === "honoredCitizen") && (this.times ==="11:00am")) {
-    return 4;
+  else if ((this.age === "honoredCitizen") && (this.times ==="matinee")) {
+    return this.ticketPrice = 4;
   }
   else {
-    return 10;
+    return this.ticketPrice = 10;
   }
 };
+
+
+$(document).ready(function(){
+  $("form#movie").submit(function(event){
+
+  var movieName = $("#movie-name").val();
+  var movieTime = $("#movie-time").val();
+  var patronAge = $("#patron-age").val();
+
+  var ticket = new Ticket(movieName, movieTime, patronAge);
+
+  var ticketPrice = ticket.price();
+
+  $(".price").text(ticketPrice);
+
+
+  event.preventDefault();
+  });
+
+
+
+
+
+
+
+});
